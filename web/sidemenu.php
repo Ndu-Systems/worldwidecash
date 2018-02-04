a19535af0486d1273610fd85c93f260eb8d4254c
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-02-04 19:34:34 +0200

    creating external file for side menu

--- web/sidemenu.php
+++ web/sidemenu.php
@@ -10,11 +10,11 @@ Hello {{name}}<br>
 </p>
 
 <ul class="list-group">
-  <li class="list-group-item"><a href="Members"><span class="glyphicon glyphicon-user"></span><span class="glyphicon glyphicon-user"></span> Members <span class="badge">{{ members }}</span></a></li>
-  <li class="list-group-item"><a href="Bonuses"><span class="glyphicon glyphicon-credit-card"></span> Bonuses <span class="badge"> R {{ bonus }}</span></a></li>
+  <li class="list-group-item"><a href=""><span class="glyphicon glyphicon-user"></span><span class="glyphicon glyphicon-user"></span> Members <span class="badge">{{ members }}</span></a></li>
+  <li class="list-group-item"><a href=""><span class="glyphicon glyphicon-credit-card"></span> Bonuses <span class="badge"> R {{ bonus }}</span></a></li>
   <li class="list-group-item"><a href="Give-Help"><span class="glyphicon glyphicon-heart-empty"></span> Donate</a></li>
   <li class="list-group-item"><a href="Dashboard"><span class="glyphicon glyphicon-th"></span> Dashboard</a></li>
-  <li class="list-group-item"><a href="History"><span class="glyphicon glyphicon-th-list"></span> History</a></li>
+  <li class="list-group-item"><a href=""><span class="glyphicon glyphicon-th-list"></span> History</a></li>
   <li class="list-group-item"><a href="Logout"><span class="glyphicon glyphicon-off"></span> Logout</a></li>
 </ul>
 </div>
