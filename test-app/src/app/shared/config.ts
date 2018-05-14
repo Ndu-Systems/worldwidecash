749bd989221e17ab394777151c24845e72301bc6
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-05-14 11:37:23 +0200

    install google icons - font awesome

--- test-app/src/app/shared/config.ts
+++ test-app/src/app/shared/config.ts
@@ -1,5 +1,5 @@
-//export const   API_URL='http://localhost:8080/worldwidecash/api';
-export const   API_URL='https://www.funderslife.com/api1';
+export const   API_URL='http://localhost:8080/worldwidecash/api';
+//export const   API_URL='https://www.funderslife.com/api1';
 
 export const EMAIL= "https://www.funderslife.com/api/emailClient2.php";
 
