6a51c996b0a490cd7679f894834006f26a9710be
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-05-19 19:12:13 +0200

    include email

--- api/automate/MatureDreams.php
+++ api/automate/MatureDreams.php
@@ -3,6 +3,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 require "../conn.php";
+require "../emailClientApiSide.php";
+
 $data   = json_decode(file_get_contents("php://input"));
 $rows   = array();
 $response = array();
